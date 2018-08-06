ca743b84f98978dfa311991f28460958921ccec0
huangshunbo <737649308@qq.com>
2018-08-06 11:48:51 +0800

    完善sample 加入README.md

--- reloadtipview/README.md
+++ reloadtipview/README.md
@@ -1,6 +1,10 @@
-# reloadtipview
-Smart reloadtipview
+#### Summary
+---
+情感图库,包含一个图标Icon，一个提示语，一个功能按键。包含四种状态：无网络、无数据、其他异常、自定义
 
+#### Getting Started
+---
+加入依赖
 ```Java
 allprojects {
   repositories {
@@ -10,6 +14,56 @@ allprojects {
 }
 
 dependencies {
-  compile 'com.github.huangshunbo:reloadtipview:1.0.0'
+  compile 'com.github.huangshunbo:reloadtipview:lastest.release'
 }
 ```
+覆盖reloadtip_default_attr设置全局style
+```Java
+<style name="reloadtip_default_attr" parent="Theme.AppCompat.Light.NoActionBar">
+    <item name="tip_txt_size">18dp</item>
+    <item name="action_txt_size">15dp</item>
+    <item name="tip_txt_color">#333333</item>
+    <item name="action_txt_color">#999999</item>
+    <item name="tip_txt_topmargin">20dp</item>
+    <item name="action_txt_topmargin">15dp</item>
+    <item name="content_minheigh">400dp</item>
+    <item name="content_topmargin">-30dp</item>
+
+    <item name="notdata_icon">@mipmap/default_no_data</item>
+    <item name="notdata_tip_txt">真的暂无数据</item>
+    <item name="notdata_actoin_txt">点击刷新</item>
+    <item name="notnet_icon">@mipmap/default_no_network</item>
+    <item name="notnet_tip_txt">网络不给力，请稍候重试</item>
+    <item name="notnet_actoin_txt">点击刷新</item>
+    <item name="error_icon">@mipmap/default_no_network</item>
+    <item name="error_tip_txt">服务器异常</item>
+    <item name="error_actoin_txt">点击刷新</item>
+</style>
+```
+ReloadTipView暴露的方法
+```Java
+reloadTipView.getIvIc();
+reloadTipView.getTvTip();
+reloadTipView.getTvAction();
+
+reloadTipView.setTipTxt("随机改变参数的Tip");
+reloadTipView.setTipTxtSize(dip2px(tipTextSize));
+reloadTipView.setTipTxtColor(tipTextColor);
+reloadTipView.setTipTopMargin(dip2px(tipTextTopMargin));
+
+reloadTipView.setActionTxt("随机改变参数的Action");
+reloadTipView.setActionTxtSize(dip2px(actionTextSize));
+reloadTipView.setActionTxtColor(actionTextColor);
+reloadTipView.setActionTopMargin(dip2px(actionTextTopMargin));
+
+reloadTipView.setContentTopMargin(dip2px(contentTopMargin));
+reloadTipView.setContentMinHeigh(dip2px(contentMinHeigh));
+
+```
+
+#### Known Issues
+---
+暂时没有收到任何反馈，有任何疑问或需求，可提issue。
+#### Support
+---
+黄顺波
--- sample-simplewidgets/src/main/java/com/android/minlib/samplesimplewidget/ReloadTipViewActivity.java
+++ sample-simplewidgets/src/main/java/com/android/minlib/samplesimplewidget/ReloadTipViewActivity.java
@@ -1,33 +1,142 @@
 package com.android.minlib.samplesimplewidget;
 
 import android.app.Application;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.minlib.reloadtipview.EmotionBean;
 import com.android.minlib.reloadtipview.ReloadTipView;
+
+import java.util.Random;
 
 public class ReloadTipViewActivity extends AppCompatActivity{
 
     ReloadTipView reloadTipView;
     int heigh = 300;
-    int tmp = 10;
+    EmotionBean bean = new EmotionBean();
+
+    TextView tvTip;
+    Button btChange;
+
+    private int[] colors = {Color.RED,Color.BLUE,Color.BLACK,Color.GREEN,Color.GRAY};
+    private String[] colorsTxt = new String[]{"红色","蓝色","黑色","绿色","灰色"};
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_reloadtipview);
         reloadTipView = findViewById(R.id.reloadtipview);
+        tvTip = findViewById(R.id.tip);
+        btChange = findViewById(R.id.change);
+
         reloadTipView.setActionClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                heigh += 100;
-                reloadTipView.setContentMinHeigh(heigh);
+                Toast.makeText(ReloadTipViewActivity.this, "唰唰唰", Toast.LENGTH_SHORT).show();
             }
         });
-        reloadTipView.showNotData();
+        reloadTipView.showNotNet();
 
+        bean.seteIcon(ContextCompat.getDrawable(this,R.mipmap.default_no_data));
+        bean.seteTipTxt("这是一个自定义的提示");
+        bean.seteActionTxt("点我唰唰唰");
+
+        btChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random random = new Random();
+                int tipTextSize = random.nextInt(20) + 10;
+                int tmp = random.nextInt(4);
+                int tipTextColor = colors[tmp];
+                String tipTextColorTxt = colorsTxt[tmp];
+                int tipTextTopMargin = random.nextInt(30);
+
+                int actionTextSize = random.nextInt(20) + 10;
+                tmp = random.nextInt(4);
+                int actionTextColor = colors[tmp];
+                String actionTextColorTxt = colorsTxt[tmp];
+                int actionTextTopMargin = random.nextInt(30);
+
+                int contentTopMargin = random.nextInt(400);
+                int contentMinHeigh = (random.nextInt(200) + 300);
+
+                StringBuffer stringBuffer = new StringBuffer();
+                stringBuffer.append("提示语 文字大小 范围10~30dp : " + tipTextSize + "\n");
+                stringBuffer.append("提示语 文字颜色 五色随机 : " + tipTextColorTxt + "\n");
+                stringBuffer.append("提示语 顶部距离 范围0~30dp : " + tipTextTopMargin + "\n");
+
+                stringBuffer.append("功能键 文字大小 范围10~30dp : " + actionTextSize + "\n");
+                stringBuffer.append("功能键 文字颜色 五色随机 : " + actionTextColorTxt + "\n");
+                stringBuffer.append("功能键 顶部距离 范围0~30dp : " + actionTextTopMargin + "\n");
+
+                stringBuffer.append("整体内容 顶部距离 范围0~400dp : " + contentTopMargin + "\n");
+                stringBuffer.append("整体内容 最小高度 范围300~500dp : " + contentMinHeigh + "\n");
+
+
+
+
+                reloadTipView.setTipTxt("随机改变参数的Tip");
+                reloadTipView.setTipTxtSize(dip2px(tipTextSize));
+                reloadTipView.setTipTxtColor(tipTextColor);
+                reloadTipView.setTipTopMargin(dip2px(tipTextTopMargin));
+
+                reloadTipView.setActionTxt("随机改变参数的Action");
+                reloadTipView.setActionTxtSize(dip2px(actionTextSize));
+                reloadTipView.setActionTxtColor(actionTextColor);
+                reloadTipView.setActionTopMargin(dip2px(actionTextTopMargin));
+
+                reloadTipView.setContentTopMargin(dip2px(contentTopMargin));
+                reloadTipView.setContentMinHeigh(dip2px(contentMinHeigh));
+
+                tvTip.setText(stringBuffer);
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("显示无网络");
+        menu.add("显示无数据");
+        menu.add("显示服务器异常");
+        menu.add("显示自定义");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(reloadTipView != null){
+            if(item.getTitle().equals("显示无网络")){
+                reloadTipView.showNotNet();
+            }else if(item.getTitle().equals("显示无数据")){
+                reloadTipView.showNotData();
+            }else if(item.getTitle().equals("显示服务器异常")){
+                reloadTipView.showError();
+            }else if(item.getTitle().equals("显示自定义")){
+
+                reloadTipView.showCustomEmotion(bean);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public int dip2px(float dipValue) {
+        final float scale = getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
     }
 }
